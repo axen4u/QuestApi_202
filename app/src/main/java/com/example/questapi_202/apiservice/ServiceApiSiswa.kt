a7f667113623d5d744474b73daa643454ac56964
axen4u <damaraarya1303@gmail.com>
2025-12-18 18:39:51 +0700

    Menambahkan Fungsi GET (Read Data)

--- app/src/main/java/com/example/questapi_202/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_202/apiservice/ServiceApiSiswa.kt
@@ -1,8 +1,17 @@
 package com.example.questapi_202.apiservice
 
+import com.example.questapi_202.modeldata.DataSiswa
+import retrofit2.http.GET
+
 /**
  * Interface layanan API untuk mengelola data siswa.
  */
 interface ServiceApiSiswa {
-    // Endpoints will be added here
+
+    /**
+     * Mengambil data siswa dari API.
+     * Endpoint: bacaTeman.php
+     */
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
 }
