109f979e2591aa41629e0199c0259035fab44536
axen4u <damaraarya1303@gmail.com>
2025-12-18 18:40:10 +0700

    Menambahkan Fungsi POST (Insert Data)

--- app/src/main/java/com/example/questapi_202/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_202/apiservice/ServiceApiSiswa.kt
@@ -1,7 +1,10 @@
 package com.example.questapi_202.apiservice
 
 import com.example.questapi_202.modeldata.DataSiswa
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 /**
  * Interface layanan API untuk mengelola data siswa.
@@ -14,4 +17,13 @@ interface ServiceApiSiswa {
      */
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
+
+    /**
+     * Mengirim data siswa baru ke API.
+     * Endpoint: insertTM.php
+     */
+    @POST("insertTM.php")
+    suspend fun postSiswa(
+        @Body dataSiswa: DataSiswa
+    ): Response<Void>
 }
