a3ec1df5ea792e98ac832793a8403b019f92f076
axen4u <damaraarya1303@gmail.com>
2025-12-25 22:35:09 +0700

    Konfigurasi Retrofit dan Gson

--- app/src/main/java/com/example/questapi_202/repositori/ContainerAPP.kt
+++ app/src/main/java/com/example/questapi_202/repositori/ContainerAPP.kt
@@ -26,3 +26,22 @@ class DefaultContainerApp : ContainerApp {
     override val repositoryDataSiswa: RepositoryDataSiswa
         get() = TODO("Not yet implemented")
 }
+
+// ... (Interface ContainerApp)
+
+import okhttp3.OkHttpClient
+
+// 2. Implementasi Default Container
+class DefaultContainerApp : ContainerApp {
+
+    // Alamat IP Loopback Emulator Android (mengarah ke localhost komputer)
+    private val baseurl = "http://10.0.2.2/umyTI/"
+
+    // Client HTTP dasar
+    private val klien = OkHttpClient.Builder()
+        .build()
+
+    // Properti lainnya akan ditambahkan pada commit berikutnya
+    override val repositoryDataSiswa: RepositoryDataSiswa
+        get() = TODO("Not yet implemented")
+}
