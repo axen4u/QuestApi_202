eb361fb586a7fd9c146338b69009721fb768edbc
axen4u <damaraarya1303@gmail.com>
2025-12-25 23:30:31 +0700

    Mendefinisikan UI State (Interface)

--- app/src/main/java/com/example/questapi_202/viewmodel/HomeviewModel.kt
+++ app/src/main/java/com/example/questapi_202/viewmodel/HomeviewModel.kt
@@ -1,7 +1,15 @@
 package com.example.questapi_202.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.questapi_202.modeldata.DataSiswa
+
+// Status UI untuk menentukan tampilan di layar
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa>) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
 
 class HomeViewModel : ViewModel() {
-    // Logic akan ditambahkan bertahap
+    // ...
 }
