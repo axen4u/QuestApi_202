85c2c9362023e0880b558e875521280d5a07ab96
axen4u <damaraarya1303@gmail.com>
2025-12-25 22:50:11 +0700

    Membuat Model Data Utama (Network Model)

--- app/src/main/java/com/example/questapi_202/modeldata/datasiswa.kt
+++ app/src/main/java/com/example/questapi_202/modeldata/datasiswa.kt
@@ -1,35 +1,12 @@
 package com.example.questapi_202.modeldata
 
-// Hapus import kotlinx.serialization.Serializable jika menggunakan Gson
-// import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializable
 
-// Hapus anotasi @Serializable jika menggunakan Gson
+@Serializable
 data class DataSiswa(
-    val id : Int,
-    val nama : String,
-    val alamat : String,
-    val telpon : String
-)
-
-
-data class DetailSiswa(    val id: Int = 0,
-                           val nama: String = "",
-                           val alamat: String = "",
-                           val telpon: String = ""
-)
-
-
-data class UIStateSiswa(
-    val detailSiswa: DetailSiswa = DetailSiswa(),
-    val isEntryValid: Boolean = false
-)// ... (kode DetailSiswa)
-
-
-
-fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    telpon = telpon
+    val id: Int,
+    val nama: String,
+    val alamat: String,
+    val telpon: String
 )
 
