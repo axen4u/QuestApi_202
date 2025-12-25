b79bd992393c9cb57877ee6654c5f58568646353
axen4u <damaraarya1303@gmail.com>
2025-12-25 22:45:22 +0700

    Menambahkan Endpoint GET by ID (Read Detail)

--- app/src/main/java/com/example/questapi_202/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_202/apiservice/ServiceApiSiswa.kt
@@ -5,6 +5,15 @@ import retrofit2.Response
 import retrofit2.http.GET // Tambah import ini
 import retrofit2.http.Body // Tambah import ini
 import retrofit2.http.POST // Tambah import ini
+// ... imports sebelumnya
+import retrofit2.http.Query // Tambah import ini
+
+interface ServiceApiSiswa {
+    // ... fungsi sebelumnya
+
+    @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+}
 
 interface ServiceApiSiswa {
 
