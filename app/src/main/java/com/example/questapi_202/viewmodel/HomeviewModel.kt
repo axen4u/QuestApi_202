75c2119c0ea767b1ed6bfb584c77a1d16c6156df
axen4u <damaraarya1303@gmail.com>
2025-12-25 23:33:30 +0700

    Menambahkan Dependency Injection Repository

--- app/src/main/java/com/example/questapi_202/viewmodel/HomeviewModel.kt
+++ app/src/main/java/com/example/questapi_202/viewmodel/HomeviewModel.kt
@@ -2,6 +2,18 @@ package com.example.questapi_202.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.questapi_202.modeldata.DataSiswa
+// ... imports
+import com.example.questapi_202.repositori.RepositoryDataSiswa
+
+sealed interface StatusUiSiswa {
+    // ... (kode interface tetap sama)
+}
+
+// Menambahkan parameter konstruktor
+class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+
+}
+
 
 // Status UI untuk menentukan tampilan di layar
 sealed interface StatusUiSiswa {
