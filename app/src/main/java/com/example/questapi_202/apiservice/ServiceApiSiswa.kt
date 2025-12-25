b54e5d6a2c780934986855935c5c8bc4baace60f
axen4u <damaraarya1303@gmail.com>
2025-12-25 22:43:56 +0700

    Menambahkan Endpoint GET (Read All)

--- app/src/main/java/com/example/questapi_202/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_202/apiservice/ServiceApiSiswa.kt
@@ -2,8 +2,10 @@ package com.example.questapi_202.apiservice
 
 import com.example.questapi_202.modeldata.DataSiswa
 import retrofit2.Response
-// Import HTTP methods akan ditambahkan sesuai kebutuhan nanti
+import retrofit2.http.GET // Tambah import ini
 
 interface ServiceApiSiswa {
-    // Endpoints akan ditambahkan secara bertahap
+
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
 }
