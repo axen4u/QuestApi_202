502fe5649cc205ef4680d7e386a1f65540113ab1
axen4u <damaraarya1303@gmail.com>
2025-12-25 22:34:30 +0700

    Setup Konfigurasi Dasar Jaringan

--- app/src/main/java/com/example/questapi_202/repositori/ContainerAPP.kt
+++ app/src/main/java/com/example/questapi_202/repositori/ContainerAPP.kt
@@ -7,3 +7,22 @@ package com.example.questapi_202.repositori
 interface ContainerApp {
     val repositoryDataSiswa: RepositoryDataSiswa
 }
+
+// ... (Interface ContainerApp)
+
+import okhttp3.OkHttpClient
+
+// 2. Implementasi Default Container
+class DefaultContainerApp : ContainerApp {
+
+    // Alamat IP Loopback Emulator Android (mengarah ke localhost komputer)
+    private val baseurl = "http://10.0.2.2/umyTI/"
+
+    // Client HTTP dasar
+    private val klien = OkHttpClient.Builder()
+        .build()
+
+    // Properti lainnya akan ditambahkan pada commit berikutnya
+    override val repositoryDataSiswa: RepositoryDataSiswa
+        get() = TODO("Not yet implemented")
+}
