24cd236232f4c788d5e50aaac5dccdbccd5d63d0
axen4u <damaraarya1303@gmail.com>
2025-12-25 22:34:12 +0700

    Membuat Kontrak Interface (DI Blueprint)

--- app/src/main/java/com/example/questapi_202/repositori/ContainerAPP.kt
+++ app/src/main/java/com/example/questapi_202/repositori/ContainerAPP.kt
@@ -1,6 +1,9 @@
 package com.example.questapi_202.repositori
 
-
-interface AppContainer {
+/**
+ * Interface ContainerApp berfungsi sebagai wadah untuk dependensi aplikasi.
+ * Menyediakan akses abstrak ke repositori.
+ */
+interface ContainerApp {
     val repositoryDataSiswa: RepositoryDataSiswa
 }
