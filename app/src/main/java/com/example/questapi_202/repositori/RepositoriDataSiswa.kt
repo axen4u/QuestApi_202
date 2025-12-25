f7e7fd1dc90f3e4102a18c0737b97a2c3c9649d8
axen4u <damaraarya1303@gmail.com>
2025-12-25 22:58:07 +0700

    Menambahkan Metode GET (Read All) ke Interface

--- app/src/main/java/com/example/questapi_202/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/questapi_202/repositori/RepositoriDataSiswa.kt
@@ -5,3 +5,8 @@ import com.example.questapi_202.modeldata.DataSiswa
 interface RepositoryDataSiswa {
     // Fungsi CRUD akan ditambahkan bertahap
 }
+// ... imports
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+}
