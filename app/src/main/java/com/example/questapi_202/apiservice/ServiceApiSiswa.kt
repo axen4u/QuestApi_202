72e4291f76304b730f6c1394050d6d370e812905
axen4u <damaraarya1303@gmail.com>
2025-12-25 22:44:41 +0700

    Menambahkan Endpoint POST (Create)

--- app/src/main/java/com/example/questapi_202/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_202/apiservice/ServiceApiSiswa.kt
@@ -3,6 +3,18 @@ package com.example.questapi_202.apiservice
 import com.example.questapi_202.modeldata.DataSiswa
 import retrofit2.Response
 import retrofit2.http.GET // Tambah import ini
+import retrofit2.http.Body // Tambah import ini
+import retrofit2.http.POST // Tambah import ini
+
+interface ServiceApiSiswa {
+
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): Response<Void>
+}
+
 
 interface ServiceApiSiswa {
 
