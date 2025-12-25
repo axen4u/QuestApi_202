99edd0b80a808acef9915b0ac3b30421a07ceb6f
axen4u <damaraarya1303@gmail.com>
2025-12-25 22:50:35 +0700

    Menambahkan Model Form (Detail)

--- app/src/main/java/com/example/questapi_202/modeldata/datasiswa.kt
+++ app/src/main/java/com/example/questapi_202/modeldata/datasiswa.kt
@@ -10,3 +10,10 @@ data class DataSiswa(
     val telpon: String
 )
 
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
